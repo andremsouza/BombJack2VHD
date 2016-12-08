c58066f80123a17d001ab4f83eac5446e9690a81
fcontinitorres <fcontinitorres@gmail.com>
2016-12-08 20:12:11 -0200

    Added support for multiple levels.
    Needs 2 enemies, 1 level, bomb collision and score.

--- keyviewer.vhd
+++ keyviewer.vhd
@@ -15,9 +15,9 @@ entity keyviewer is
 end keyviewer;
 
 architecture behav of keyviewer is
-	type bg is array (1199 downto 0) of std_logic_vector(15 downto 0);
-	type lvlpf is array (1 downto 0) of std_logic_vector(1199 downto 0); --PLATFORMS
-	type lvlbb is array (1 downto 0) of std_logic_vector(1199 downto 0); -- BOMBS
+	type bg is array (0 to 1199) of std_logic_vector(15 downto 0);
+	type lvlpf is array (1 downto 0) of std_logic_vector(0 to 1199); --PLATFORMS
+	type lvlbb is array (1 downto 0) of std_logic_vector(0 to 1199); -- BOMBS
 	type lvlbn is array (1 downto 0) of integer;
 	type lvlip is array (1 downto 0) of std_logic_vector(15 downto 0); -- INITIAL POSITION
 	type lvlep0 is array (1 downto 0) of std_logic_vector(15 downto 0); -- ENEMY POSITION
@@ -34,50 +34,39 @@ architecture behav of keyviewer is
 	signal JUMPSTATE: std_logic_vector(7 downto 0);
 	
 	signal LVLE: std_logic_vector(3 downto 0) := x"0";
-	signal PLATFORM: lvlpf := (0 => (172 downto 162|504 downto 493|796 downto 789|970 downto 963 => '1', others => '0'), 1 => (others => '0')); -- Falta nivel 2
+	signal PLATFORM: lvlpf := (0 => (0 to 39|1160 to 1199|172 downto 162|504 downto 493|796 downto 789|970 downto 963 => '1', others => '0'), 1 => (others => '0')); -- Falta nivel 2
 	signal BOMB: lvlbb := (0 => (203|206|209|319|320|455|458|461|479|480|535|538|541|639|640|799|800|926 => '1', others => '0'), 1 => (others => '0')); -- Falta nivel 2
-	signal BNUMBER :lvlbn := (18, 0); -- Falta nível 2
+	signal BNUMBER :lvlbn := (18, 0); -- Falta nÃ­vel 2
 	signal JACKPOSI: lvlip := (0 => (x"0072"), 1 => (others => '0')); -- Falta nivel 2
 	signal ENEMY0: lvlep0 := (0 => (x"007F"), 1 => (others => '0')); -- Falta nivel 2
 	signal ENEMY1: lvlep0 := (0 => (x"0481"), 1 => (others => '0')); -- Falta nivel 2
 	
-	signal BACKGROUND: bg := (others => (15 downto 12|11 downto 8|7|1 => '0', others => '1'));
+	signal BACKGROUND: bg;
 	signal BGMAKE: std_logic := '0';
 	signal BGDRAW: std_logic := '0';
 begin
-	process(clk, reset) -- Level Control
-	begin
-		
-	end process;
-	
-	process(clk, reset) -- Enemy 0 Control
-	begin
-		
-	end process;
-	process(clk, reset) -- Enemy 1 Control
-	begin
-		
-	end process;
 	process(clk, reset) -- Draw Background
 	begin
 		if(reset = '1') then
 			BGMAKE <= '0';
 			BACKGROUND <= (others => (15 downto 12|11 downto 8|7|1 => '0', others => '1'));
-		end if;
-		if(BGMAKE = '0') then
-			for I in 0 to 1199 loop
-				if(PLATFORM(I) = '1') then
-					BACKGROUND(I)(15 downto 12) <= x"0";
-					BACKGROUND(I)(11 downto 8) <= x"B";
-					BACKGROUND(I)(7 downto 0) <= x"02";
-				elsif(BOMB(I) = '1') then
-					BACKGROUND(I)(15 downto 12) <= x"0";
-					BACKGROUND(I)(11 downto 8) <= x"9";
-					BACKGROUND(I)(7 downto 0) <= x"2A";
-					--BACKGROUND(I) <= "0000000001111101";
-				end if;
-			end loop;
-			BGMAKE <= '1';
+		elsif(clk'event and clk='1') then
+			if(BGMAKE = '0') then
+				for I in 0 to 1199 loop
+					if(PLATFORM(conv_integer(LVLE))(I) = '1') then
+						BACKGROUND(I)(15 downto 12) <= x"0";
+						BACKGROUND(I)(11 downto 8) <= x"3";
+						BACKGROUND(I)(7 downto 0) <= x"02";
+					elsif(BOMB(conv_integer(LVLE))(I) = '1') then
+						BACKGROUND(I)(15 downto 12) <= x"0";
+						BACKGROUND(I)(11 downto 8) <= x"9";
+						BACKGROUND(I)(7 downto 0) <= x"2A";
+					else
+						BACKGROUND(I) <= "0000000001111101";
+					end if;
+				end loop;
+				BGMAKE <= '1';
+			end if;
 		end if;
 	end process;
 
@@ -88,23 +77,23 @@ begin
 		if(reset = '1') then
 			JACKCHAR <= x"01";
 			JACKCOLOR <= x"C";
-			JACKPOS <= x"026B";
+			JACKPOS <= JACKPOSI(conv_integer(LVLE));
 			JDELAY <= x"00000000";
 			JUMPSTATE <= x"00";
 			JACKSTATE <= x"00";
 		elsif (clk'event and clk = '1') then
-			if(PLATFORM(conv_integer(JACKPOS) + 40) = '0' and JUMPSTATE = x"00") then
+			if(PLATFORM(conv_integer(LVLE))(conv_integer(JACKPOS) + 40) = '0' and JUMPSTATE = x"00") then
 				JUMPSTATE <= x"02";
 			end if;
 			case JUMPSTATE is -- Controle de pulo
 				when x"00" => -- Parado (vertical)
 				when x"01" => -- Subindo
-					if(PLATFORM(conv_integer(JACKPOS) - 40) = '0') then
+					if(PLATFORM(conv_integer(LVLE))(conv_integer(JACKPOS) - 2*40) = '0') then
 						if(delayj2 >= x"00000010") then --ajustar
 							delayj2 := x"00000000"; --ajustar
 							JUMPSTATE <= x"02";
 						else
-							if(delayj1 >= x"00000FFF") then --ajustar
+							if(delayj1 >= x"000023FF") then --ajustar
 								delayj1 := x"00000000"; -- ajustar
 								delayj2 := delayj2 + x"01";
 								JACKPOS <= JACKPOS - x"28";
@@ -118,8 +107,8 @@ begin
 						JUMPSTATE <= x"02";
 					end if;
 				when x"02" => -- Caindo
-					if(PLATFORM(conv_integer(JACKPOS) + 40) = '0') then
-						if(delayj1 >= x"00000FFF") then -- ajustar
+					if(PLATFORM(conv_integer(LVLE))(conv_integer(JACKPOS) + 40) = '0') then
+						if(delayj1 >= x"000037FF") then -- ajustar
 							delayj1 := x"00000000"; --ajustar
 							JACKPOS <= JACKPOS + x"28";
 						else
@@ -134,7 +123,7 @@ begin
 				when x"00" =>
 					case key is
 						when x"61"|x"41" => -- A -- Esquerda
-							if(not((conv_integer(JACKPOS) mod 40) = 0) and PLATFORM(conv_integer(JACKPOS) - 1) = '0') then
+							if(not((conv_integer(JACKPOS) mod 40) = 0) and PLATFORM(conv_integer(LVLE))(conv_integer(JACKPOS) - 1) = '0') then-- and PLATFORM(conv_integer(LVLE))(conv_integer(JACKPOS) - 41) = '0') then
 								JACKPOS <= JACKPOS - x"01";
 							end if;
 						when x"20"|x"77" => -- Space -- new
@@ -142,14 +131,14 @@ begin
 								JUMPSTATE <= x"01";
 							end if; -- end new
 						when x"64"|x"44" => -- D -- Direita
-							if(not((conv_integer(JACKPOS) mod 40) = 39) and PLATFORM(conv_integer(JACKPOS) + 1) = '0') then
+							if(not((conv_integer(JACKPOS) mod 40) = 39) and PLATFORM(conv_integer(LVLE))(conv_integer(JACKPOS) + 1) = '0' )then-- and PLATFORM(conv_integer(LVLE))(conv_integer(JACKPOS) - 39) = '0') then
 								JACKPOS <= JACKPOS + x"01";
 							end if;		
 						when others =>
 					end case;
 					JACKSTATE <= x"01";
 				when x"01" =>
-					if JDELAY >= x"00005FFF" then --ajustar
+					if JDELAY >= x"00000FFF" then --ajustar
 						JDELAY <= x"00000000"; --ajustar
 						JACKSTATE <= x"00";
 					else
@@ -166,7 +155,7 @@ begin
 		if (reset='1') then
 			VIDEOE <= x"00";
 			videodraw <= '0';
-			JACKPOSA <= x"0000";
+			--JACKPOSA <= x"0000";
 			BGDRAW <= '0';
 		elsif (clk'event and clk = '1') then
 			case VIDEOE is
